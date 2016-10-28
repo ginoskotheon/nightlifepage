26a82ebeb6de433c80e83e488aca44dabbf416bf
William McDonald <ginoskotheon@yahoo.com>
2016-10-28 09:20:27 -0500

    button fix

--- public/assets/macquery.js
+++ public/assets/macquery.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
 
   $('#findButton').on('click', function(){
               var myloc = $('#location').val();
-  });          
+  });
+
+  $(document).on('click', '#decision', function(){
+    console.log($(this).val());
+    if($(this).val()==='Not Going'){
+      $(this).val('Going');
+    }else {
+      $(this).val('Not Going');
+    }
+
+  });
 });
