fffefbb39934efad06a7bb75ee9766ed0e956f71
William McDonald <ginoskotheon@yahoo.com>
2016-10-28 12:13:39 -0500

    button fix

--- models/users.js
+++ models/users.js
@@ -4,11 +4,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-	twitter: {
-    consumerKey: String,
-    consumerSecret: String,
-    callbackURL: String
-  },
+  user: String,
+	id: String,
 	venues: []
 });
 
