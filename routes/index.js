3d84075264e606a800ebc2136404a536221ab75e
William McDonald <ginoskotheon@yahoo.com>
2016-11-22 15:35:14 -0600

    clean up

--- routes/index.js
+++ routes/index.js
@@ -57,7 +57,7 @@ router.get('/login', function(req, res, next){
 router.get('/auth/twitter', passportTwitter.authenticate('twitter'));
 
 router.get('/auth/twitter/callback',
-  passportTwitter.authenticate('twitter', {successRedirect : '/twitter', failureRedirect: '/login' }),
+  passportTwitter.authenticate('twitter', {successRedirect : '/eventslogged', failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication
     console.log('success!');
